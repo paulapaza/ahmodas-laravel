9031f3674ad9c75b33f251ed5c7286c5767818e2
Jorge Ccallo <jccallo@hyosystem.com>
2026-04-09 10:58:20 -0500

    feat: add inventory menu component and implement sidebar layout partial

--- resources/views/components/menuInventario.blade.php
+++ resources/views/components/menuInventario.blade.php
@@ -13,6 +13,7 @@
         
     </ul>
 </li>   --}}
+@unless(auth()->user()->hasRole('cajero'))
 <li class="nav-item d-none d-sm-inline-block">
     <a href="{{route('inventario.productos.index')}}" class="nav-link">Productos</a>
 </li>
@@ -22,27 +23,21 @@
 <li class="nav-item d-none d-sm-inline-block">
     <a href="{{route('inventario.salidas.index')}}" class="nav-link">Stock</a>
 </li>
+@endunless
 
 <li class="nav-item dropdown">
     <a id="dropdownTransacciones" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
         class="nav-link dropdown-toggle">Transacciones</a>
     <ul aria-labelledby="dropdownTransacciones" class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
+        @unless(auth()->user()->hasRole('cajero'))
         <li><a href="{{route('inventario.kardex.index')}}" class="dropdown-item">Movimientos (Kardex)</a></li>
         <li><a href="{{route('inventario.transacciones.index')}}" class="dropdown-item">Gestión de Stock</a></li>
+        @endunless
         <li><a href="{{ auth()->user()->hasRole('cajero') ? route('inventario.traslados_almacen.historial') : route('inventario.traslados_almacen.index') }}" class="dropdown-item">Traslados desde Almacén</a></li>
     </ul>
 </li>
 
-{{-- <li class="nav-item dropdown">
-    <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
-        class="nav-link dropdown-toggle">Informes</a>
-    <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow" style="left: 0px; right: inherit;">
-        <li><a href="#" class="dropdown-item">Inventario</a></li>
-        <li><a href="#" class="dropdown-item">Moviento de productos</a></li>
-        <li class="dropdown-divider"></li>
-        <li><a href="#" class="dropdown-item">Valoracion de Inventario</a></li>
-    </ul>
-</li>  --}}   
+@unless(auth()->user()->hasRole('cajero'))
 <li class="nav-item dropdown">
     <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"
         class="nav-link dropdown-toggle">Configuracion</a>
@@ -54,3 +49,4 @@
         <li><a href="{{route('inventario.marcas.index')}}" class="dropdown-item">Marcas de Producto</a></li>
         </ul>
 </li>
+@endunless
