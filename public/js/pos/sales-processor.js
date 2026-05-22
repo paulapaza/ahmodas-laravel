894f349a3e28d92b73ef8866800db9468cfd1f83
Jorge Ccallo <jccallo@outlook.com>
2026-05-21 21:11:15 -0500

    fix: se activa sincronizacion

--- public/js/pos/sales-processor.js
+++ public/js/pos/sales-processor.js
@@ -240,7 +240,7 @@ class SalesProcessor {
         console.log('data-libre:', data);
         // descomentar en produccion
         await POSUtils.makeAjaxRequest(
-          'http://127.0.0.1:8001/api/sync/orders',
+          'https://ahmodas.com/v2/api/sync/orders',
           data
         )
         .then(res => console.log('respuesta', res))
